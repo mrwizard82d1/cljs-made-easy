f96e7950d236b6b079ed796991a6f0cbdc3f1f2d
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-10-25 15:32:53 -0500

    Add messages when building for node.js.

--- scripts/node.clj
+++ scripts/node.clj
@@ -1,8 +1,10 @@
 (require 'cljs.build.api)
 
-(cljs.build.api/build "src"
-                      {:main 'time-calc.core
-                       :output-to "out/time_calc.js"
-                       :output-dir "out"
-                       :target :nodejs})
+(let [start (System/nanoTime)]
+  (cljs.build.api/build "src"
+                        {:main       'time-calc.core
+                         :output-to  "out/time_calc.js"
+                         :output-dir "out"
+                         :target     :nodejs})
+  (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
 
