f9a84217abd6a8656e17291d441bed1d71249583
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2015-10-25 21:14:15 -0500

    Write transform function (broken?).
    Write the transform function. I think I have written the same function
    that was in the blog post; however, when I run it, the code fails at
    run-time because of an undefined value. However, if I print the value of
    line, which I think might be undefined, everything works! Very
    perplexing.

--- scripts/build.clj
+++ scripts/build.clj
@@ -7,6 +7,7 @@
     {:main 'cljs-made-easy.core
      :output-to "out/cljs_made_easy.js"
      :output-dir "out"
+     :optimizations :whitespace
      :verbose true})
   (println "... done. Elapsed" (/ (- (System/nanoTime) start) 1e9) "seconds"))
 
---
+++ src/cljs_made_easy/line_by_line.cljs
@@ -0,0 +1,24 @@
+(ns cljs-made-easy.line-by-line)
+
+;; Require the node file system and stream modules
+(def fs (js/require "fs"))
+(def stream (js/require "stream"))
+
+;; Define the os-specific end-of-line marker
+(def ^:const eol (.-EOL (js/require "os")))
+
+
+;; Define functions to transform data in the pipeline and....
+(defn transform [chunk encoding done-fn]
+  (this-as this
+    ;; if we have a last line, append the chunk to this last line; otherwise, start with the chunk.
+    ;; notice also that invoking str on chunk, a data buffer, converts chunk to a string.
+    (let [data (if (.-lastLineData this)
+                 (str (.-lastLineData this) chunk)
+                 (str chunk))
+          lines (clojure.string/split data (js/RegExp. eol "g"))]
+      (set! (.-lastLineData this) (last lines))
+      (doseq [line (butlast lines)
+              _ (println "line" line)]
+        (.push this line))
+      (done-fn nil chunk))))
